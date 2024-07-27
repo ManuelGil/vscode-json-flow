1ccbfa5a5f3fff0c220532bfeebd9ffe0a3d109f
ManuelGil <atencion.ms@outlook.com>
2024-07-27 13:05:08 -0500

    fix: :rotating_light: fix compilation settings

--- webview/components/UpdateNode.tsx
+++ webview/components/UpdateNode.tsx
@@ -1,50 +1,39 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-import {ReactFlow, useEdgesState, useNodesState} from "@xyflow/react";
-import "@xyflow/react/dist/style.css";
+import { ReactFlow, useEdgesState, useNodesState } from '@xyflow/react';
+import '@xyflow/react/dist/style.css';
 
-const initialEdges = [{id: 'e1-2', source: 'users', target: 'name'}];
+const UpdateNode = ({ nodeList }: { nodeList: any }) => {
+  console.log('nodess', nodeList);
+  const [nodes, setNodes, onNodesChange] = useNodesState(nodeList);
+  const edge = nodeList
+    .map((node: any) => {
+      if (node.father) {
+        return {
+          id: 'e' + node.id,
+          source: node.father,
+          target: node.id,
+        };
+      } else null;
+    })
+    .filter((node: any) => node);
+  console.log('edge', edge);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(edge);
 
-
-const UpdateNode = ({nodess}: { nodess: any }) => {
-    console.log("nodess", nodess);
-    const [nodes, setNodes, onNodesChange] = useNodesState(nodess);
-    const edge = nodess.map((x) => {
-        if (x.father) {
-            return (
-                {
-                    id: 'e'+x.id,
-                    source: x.father,
-                    target: x.id,
-                }
-
-            )
-
-        }
-        else null
-    }).filter((x) => x);
-    console.log("edge", edge);
-    const [edges, setEdges, onEdgesChange] = useEdgesState(edge);
-
-
-
-    return (
-        <div className={"w-screen h-screen"}>
-            <ReactFlow
-                nodes={nodes}
-                edges={edges}
-                onNodesChange={onNodesChange}
-                onEdgesChange={onEdgesChange}
-                defaultViewport={{x: 0, y: 0, zoom: 1.5}}
-                minZoom={1}
-                maxZoom={4}
-                attributionPosition="bottom-left"
-                fitView
-                fitViewOptions={{padding: 0.5}}
-            >
-            </ReactFlow>
-
-        </div>
-    );
+  return (
+    <div className={'h-screen w-screen'}>
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        defaultViewport={{ x: 0, y: 0, zoom: 1.5 }}
+        minZoom={1}
+        maxZoom={4}
+        attributionPosition="bottom-left"
+        fitView
+        fitViewOptions={{ padding: 0.5 }}
+      ></ReactFlow>
+    </div>
+  );
 };
 
 export default UpdateNode;
