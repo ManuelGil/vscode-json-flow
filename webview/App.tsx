965fce6dbc93fe4cc7c6cac43ddebf4cb476a39a
ManuelGil <atencion.ms@outlook.com>
2024-11-12 21:44:29 -0500

    style: ✨ enhance button styling and layout in webview

--- webview/App.tsx
+++ webview/App.tsx
@@ -1,15 +1,15 @@
 import {
-	ConnectionLineType,
-	Controls,
-	MiniMap,
-	Panel,
-	ReactFlow,
-	addEdge,
-	useEdgesState,
-	useNodesState,
-	Node,
-	Edge,
-	Connection,
+  ConnectionLineType,
+  Controls,
+  MiniMap,
+  Panel,
+  ReactFlow,
+  addEdge,
+  useEdgesState,
+  useNodesState,
+  Node,
+  Edge,
+  Connection,
 } from '@xyflow/react';
 import '@xyflow/react/dist/style.css';
 import { useCallback, useEffect, useState } from 'react';
@@ -22,108 +22,109 @@ import { layoutElements } from './components/layout-elements.ts';
 const vscode = acquireVsCodeApi();
 
 const nodeTypes = {
-	custom: CustomNode,
+  custom: CustomNode,
 };
 
 const LayoutFlow = () => {
-	const [json, setJson] = useState(vscode.getState());
-	const [nodes, setNodes, onNodesChange] = useNodesState<Node>([]);
-	const [edges, setEdges, onEdgesChange] = useEdgesState<Edge>([]);
+  const [json, setJson] = useState(vscode.getState());
+  const [nodes, setNodes, onNodesChange] = useNodesState<Node>([]);
+  const [edges, setEdges, onEdgesChange] = useEdgesState<Edge>([]);
 
-	useEffect(() => {
-		const handleMessage = (event: MessageEvent) => {
-			const message = event.data;
+  useEffect(() => {
+    const handleMessage = (event: MessageEvent) => {
+      const message = event.data;
 
-			switch (message.type) {
-				case 'clearJson': {
-					setJson(null);
-					vscode.setState(null);
-					break;
-				}
+      switch (message.type) {
+        case 'clearJson': {
+          setJson(null);
+          vscode.setState(null);
+          break;
+        }
 
-				case 'setJson': {
-					setJson(message.data);
-					vscode.setState(message.data);
-					break;
-				}
+        case 'setJson': {
+          setJson(message.data);
+          vscode.setState(message.data);
+          break;
+        }
 
-				default: {
-					break;
-				}
-			}
-		};
+        default: {
+          break;
+        }
+      }
+    };
 
-		window.addEventListener('message', handleMessage);
-		return () => window.removeEventListener('message', handleMessage);
-	}, []);
+    window.addEventListener('message', handleMessage);
+    return () => window.removeEventListener('message', handleMessage);
+  }, []);
 
-	useEffect(() => {
-		if (json) {
-			const treeRootId = 1;
-			const { nodes: layoutedNodes, edges: layoutedEdges } = layoutElements(
-				json,
-				treeRootId,
-				'TB'
-			);
-			setNodes(layoutedNodes);
-			setEdges(layoutedEdges);
-		}
-	}, [json, setNodes, setEdges]);
+  useEffect(() => {
+    if (json) {
+      const treeRootId = 1;
+      const { nodes: layoutedNodes, edges: layoutedEdges } = layoutElements(
+        json,
+        treeRootId,
+        'TB'
+      );
+      setNodes(layoutedNodes);
+      setEdges(layoutedEdges);
+    }
+  }, [json, setNodes, setEdges]);
 
-	const onConnect = useCallback(
-		(params: Connection) =>
-			setEdges((eds) =>
-				addEdge(
-					{ ...params, type: ConnectionLineType.SmoothStep, animated: true },
-					eds
-				)
-			),
-		[setEdges]
-	);
+  const onConnect = useCallback(
+    (params: Connection) =>
+      setEdges((eds) =>
+        addEdge(
+          { ...params, type: ConnectionLineType.SmoothStep, animated: true },
+          eds
+        )
+      ),
+    [setEdges]
+  );
 
-	const onLayout = useCallback(
-		(direction: 'TB' | 'LR') => {
-			if (!json) {
-				return;
-			}
+  const onLayout = useCallback(
+    (direction: 'TB' | 'LR') => {
+      if (!json) {
+        return;
+      }
 
-			const treeRootId = 1;
-			const { nodes: layoutedNodes, edges: layoutedEdges } = layoutElements(
-				json,
-				treeRootId,
-				direction
-			);
+      const treeRootId = 1;
+      const { nodes: layoutedNodes, edges: layoutedEdges } = layoutElements(
+        json,
+        treeRootId,
+        direction
+      );
 
-			setNodes([...layoutedNodes]);
-			setEdges([...layoutedEdges]);
-		},
-		[json, setNodes, setEdges]
-	);
+      setNodes([...layoutedNodes]);
+      setEdges([...layoutedEdges]);
+    },
+    [json, setNodes, setEdges]
+  );
 
-	if (!json) {
-		return <Loading />;
-	}
+  if (!json) {
+    return <Loading />;
+  }
 
-	return (
-		<div className="h-screen w-screen">
-			<ReactFlow
-				nodes={nodes}
-				edges={edges}
-				onNodesChange={onNodesChange}
-				onEdgesChange={onEdgesChange}
-				onConnect={onConnect}
-				connectionLineType={ConnectionLineType.SmoothStep}
-				fitView
-				nodeTypes={nodeTypes}>
-				<Controls />
-				<MiniMap />
-				<Panel position="top-right">
-					<button onClick={() => onLayout('TB')}>vertical layout</button>
-					<button onClick={() => onLayout('LR')}>horizontal layout</button>
-				</Panel>
-			</ReactFlow>
-		</div>
-	);
+  return (
+    <div className="h-screen w-screen">
+      <ReactFlow
+        nodes={nodes}
+        edges={edges}
+        onNodesChange={onNodesChange}
+        onEdgesChange={onEdgesChange}
+        onConnect={onConnect}
+        connectionLineType={ConnectionLineType.SmoothStep}
+        fitView
+        nodeTypes={nodeTypes}
+      >
+        <Controls />
+        <MiniMap />
+        <Panel className="flex justify-between gap-2" position="top-right">
+          <button onClick={() => onLayout('TB')}>vertical layout</button>
+          <button onClick={() => onLayout('LR')}>horizontal layout</button>
+        </Panel>
+      </ReactFlow>
+    </div>
+  );
 };
 
 export default LayoutFlow;
